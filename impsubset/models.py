64b4ba2a6f6bbdce60a8a8c7706f3fbc1c23b3be
Annie Pompa <apompa@iu.edu>
2021-03-16 15:08:17 -0400

    found an issue

--- impsubset/models.py
+++ impsubset/models.py
@@ -8,15 +8,21 @@
 # something like algo 2.33?
 class Model:
 
-    def __init__(self, set, interpfn):
+    def __init__(self, set, interpn):
         self.uni = set
-        self.fn = interpfn
+        self.interpn = interpn
         # notes: wanna gen everything that has compliment in them, generate all provable ones
         # if contrad, break
         # orthoposet
         # some x are y -> x and y both state
 
-    def generate(self, nouns):
+    def generate(self):
         # of the all logic
-        sentences = [('a', n, n) for n in nouns]
+        sentences = []
+        mydict = self.interpn
+        for noun in self.interpn:
+            for othern in self.interpn:
+                if mydict[noun].issubset(mydict[othern]):
+                    sentences.append(('a', noun, othern))
+        return sentences
 
